473b8943e3b7336bc6d68dfa8b66695ba43af231
EmersonB <eberlik@gmail.com>
2017-01-13 23:59:45 -0500

    got rid of animations

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Router, Route,  hashHistory } from "react-router";
 import Portfolio from "./components/portfolio";
 import About from "./components/about";
 import Isitchristmas from "./components/isitchristmas";
-import MobileAppClub from "./components/mobileAppClub"
+import MobileAppClub from "./components/mobileAppClub";
+import Container from "./components/map";
 
 ReactDOM.render(
 <Router history={hashHistory}>
@@ -14,6 +15,7 @@ ReactDOM.render(
     <Route path="/about" component={About}/>
     <Route path="/isitchristmas" component={Isitchristmas}/>
     <Route path="/mobileappclub" component={MobileAppClub}/>
+    <Route path="/map" component={Container}/>
 
 
 
