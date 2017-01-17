0b20eea49414596e6777391be284843fb9f86803
EmersonB <eberlik@gmail.com>
2017-01-17 09:54:22 -0500

    added packages

--- src/components/map.js
+++ src/components/map.js
@@ -53,6 +53,7 @@ export class Container extends Component{
         }
         var hannibal = <div className = "header7"> St.Petersburg <br/> <div className = "h7"> Huck begins the book here with widow Watson. Huck here finds his old friend Tom Sawyer who asks him to join a gang. There is a social hierarchy in this town. The adults all hold more power than the children, especially the white adult men. </div></div>
         var mississippi = <div className = "header7"> Mississippi River <br/> <div className = "h7"> Huck and Jim are floating down the Mississippi together, Huck to get away from oppression, and Jim to get to a free state. Although they are friends, Huck has many doubts about what he is doing, and at some times, consideres turning Jim in. He later resolves to do whatever is needed to do the right thing. This relates to Marxist because Huck was raised, being taught that whites are superior to the slaves.</div></div>
+        var bainbridge = <div className = "header7"> Walter Scott Wreck <br/> <div className = "h7"> This is the wreck of the Walter Scott. Huck tells Jim about kings and dukes, and Jim, who was a runaway slave, is suprised that kings can be so powerful without having to work. On the contrary, slaves work a great deal without having any power. "They don't do nothing! Why, how you talk! They just set around" (Twain 77). </div></div>
         return (
             <Map google={this.props.google}
         initialCenter = {{lat: 39.7084, lng: -91.3585}}
@@ -70,8 +71,8 @@ export class Container extends Component{
         position={{lat: 39.620744, lng: -91.226710}} />
     <Marker
         onClick={this.onMarkerClick}
-        name={mississippi}
-        position={{lat: 39.620744, lng: -91.226710}} />
+        name={bainbridge}
+        position={{lat: 37.410616, lng: -89.437030}} />
 
             <InfoWindow
         marker={this.state.activeMarker}
